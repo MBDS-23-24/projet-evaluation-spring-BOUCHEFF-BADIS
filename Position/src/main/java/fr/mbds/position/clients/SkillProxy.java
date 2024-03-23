58476031d43d95bca60006148118de8b5a9e2327
Boucheffa <you@example.com>
2024-03-23 16:02:25 +0100

    Add discovry service

--- Position/src/main/java/fr/mbds/position/clients/SkillProxy.java
+++ Position/src/main/java/fr/mbds/position/clients/SkillProxy.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "skill", url = "http://localhost:8081/")
+@FeignClient(name = "SKILL")
 
 public interface SkillProxy {
 
