d8a191013732865a66f9202dc93bd00220290e38
Boucheffa <you@example.com>
2024-03-23 16:13:19 +0100

    Add circuit breaker

--- Position/src/main/java/fr/mbds/position/clients/SkillProxy.java
+++ Position/src/main/java/fr/mbds/position/clients/SkillProxy.java
@@ -2,6 +2,7 @@ package fr.mbds.position.clients;
 
 import fr.mbds.position.entities.Position;
 import fr.mbds.position.models.Skill;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +14,23 @@ import java.util.List;
 public interface SkillProxy {
 
     @GetMapping("/skill/{id}")
+    @CircuitBreaker(name = "skill", fallbackMethod = "getDefaultSkill")
+
     public Skill getSkillById(@PathVariable Long id);
     @GetMapping("/skills")
+    @CircuitBreaker(name = "position", fallbackMethod = "getAllPositions")
     public List<Skill> getAllSkills();
+
+    default Skill getDefaultSkill(Long id, Exception exception)
+    {
+        Skill skill = new Skill();
+        skill.setId(id);
+        skill.setName("Not available");
+        return skill;
+    }
+    default List<Skill> getAllPositions(Exception exception)
+    {
+        return List.of();
+    }
 
 }
